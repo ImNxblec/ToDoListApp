eaad2cfafa61e919394167701e36a89554b2c0b9
junior <API con repositorio>
2023-10-24 21:08:07 -0500

    Clase del Martes 24/10/2023

--- ToDoListApp/MainPage.xaml.cs
+++ ToDoListApp/MainPage.xaml.cs
@@ -1,24 +1,20 @@
-﻿namespace ToDoListApp
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoListApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        public string MyTitle { get; set; } = "Lista de tareas";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void AddBtn_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            DisplayAlert("Alert", "Agregado", "Ok");
         }
     }
 }
